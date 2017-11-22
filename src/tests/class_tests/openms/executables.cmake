46d94b5e84027cd403e778341163261b173ce43c
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2017-11-22 10:10:53 +0100

    feat: add class files with (con|de)structor and tests closes #49

--- src/tests/class_tests/openms/executables.cmake
+++ src/tests/class_tests/openms/executables.cmake
@@ -595,6 +595,7 @@ if(NOT DISABLE_OPENSWATH)
     ChromatogramExtractorAlgorithm_test
     OpenSwathHelper_test
     OpenSwathScoring_test
+    PeakIntegrator_test
     PeakPickerMRM_test
     MRMTransitionGroupPicker_test
     DIAHelper_test
